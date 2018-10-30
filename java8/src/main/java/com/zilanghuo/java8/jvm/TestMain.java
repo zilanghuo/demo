1afb34800a1db3d1baad6e1dcc23770c5e91476a
laiwf <laiwf@zendaimoney.com>
2018-10-30 17:46:31 +0800

    增加字节码

---
+++ java8/src/main/java/com/zilanghuo/java8/jvm/TestMain.java
@@ -0,0 +1,24 @@
+package com.zilanghuo.java8.jvm;
+
+/**
+ * @author laiwufa
+ * @date 2018/10/30
+ * use:
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void init(){
+
+    }
+
+    private static int simpleMethod() {
+        int x = 13;
+        int y = 14;
+        int z = x + y;
+        return z;
+    }
+}
