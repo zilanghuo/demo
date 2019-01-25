1f17f94a66a8807e2d33327fc5aad7384cb33fa4
laiwf <laiwf@zendaimoney.com>
2019-01-25 15:20:14 +0800

    重写threadPool

--- web/src/main/java/com/zilanghuo/web/utils/TraceThreadPoolTaskExecutor.java
+++ web/src/main/java/com/zilanghuo/web/utils/TraceThreadPoolTaskExecutor.java
@@ -8,18 +8,24 @@ import java.util.concurrent.RejectedExecutionException;
 
 /**
  * @author laiwufa
- * @date 2019/1/25 0025
+ * @date 2019/1/25
  */
 public class TraceThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
 
     @Override
     public void execute(Runnable task) {
         Executor executor = getThreadPoolExecutor();
+
+        LcbTraceRunnable lcbTraceRunnable = new LcbTraceRunnable() {
+            @Override
+            public void concreteRun() {
+                task.run();
+            }
+        };
+
         try {
-            System.out.println("重写");
-            executor.execute(task);
-        }
-        catch (RejectedExecutionException ex) {
+            executor.execute(lcbTraceRunnable);
+        } catch (RejectedExecutionException ex) {
             throw new TaskRejectedException("Executor [" + executor + "] did not accept task: " + task, ex);
         }
     }
