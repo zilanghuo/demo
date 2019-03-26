18f11eef5710f253b51cb612f5838dbef6ee6794
laiwf <laiwf@zendaimoney.com>
2019-03-26 17:37:15 +0800

    增加数组排序组合算法

---
+++ spring-boot-demo/src/main/java/com/zilanghuo/springbootdemo/controller/Health.java
@@ -0,0 +1,35 @@
+package com.zilanghuo.springbootdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author laiwufa
+ * @date 2019/3/26 0026 下午 5:27
+ */
+@RestController
+@Scope("singleton")
+@Slf4j
+public class Health {
+
+    @RequestMapping(value = "/health/check")
+    @ResponseBody
+    public String check() {
+        try {
+            log.info("健康检查-----------------------------------");
+            log.info("最大可用内存:" + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "M\t");
+            log.info("当前JVM空闲内存:" + Runtime.getRuntime().freeMemory() / 1024 / 1024 + "M\t");
+            log.info("当前JVM占用的内存总数:" + Runtime.getRuntime().totalMemory() / 1024 / 1024 + "M\t");
+            log.info("-----------------------------------");
+            return "OK";
+        } catch (Exception e) {
+            log.error("checkHealth error", e);
+            return "FAIL";
+        }
+
+    }
+
+}
