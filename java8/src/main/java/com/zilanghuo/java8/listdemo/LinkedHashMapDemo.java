1236706b5d4f9c55e084b6488d3b5ec48595e02c
zilanghuo <wufalai@163.com>
2019-05-07 22:58:17 +0800

    增加linkedHashMap

---
+++ java8/src/main/java/com/zilanghuo/java8/listdemo/LinkedHashMapDemo.java
@@ -0,0 +1,16 @@
+package com.zilanghuo.java8.listdemo;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author laiwufa
+ * @date 2019/5/7 21:59
+ */
+public class LinkedHashMapDemo {
+
+    public static void main(String[] args) {
+        LinkedHashMap hashMap = new LinkedHashMap();
+
+    }
+
+}
