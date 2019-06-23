a98309e0169dcdb90941812f09fdbcba5a522684
zilanghuo <wufalai@163.com>
2019-06-23 15:04:50 +0800

    增加aop 插件

--- spring-boot-demo/src/main/java/com/zilanghuo/springbootdemo/Swagger2.java
+++ spring-boot-demo/src/main/java/com/zilanghuo/springbootdemo/Swagger2.java
@@ -31,11 +31,9 @@ public class Swagger2 {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("程序猿DD")
-                .version("1.0")
+                .title("Spring Boot Swagger2")
+                .description("Spring Boot Swagger2")
+                .contact("laiwufa")
                 .build();
     }
 }
