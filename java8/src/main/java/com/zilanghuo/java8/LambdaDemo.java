51f22cf6a25b0a6683bc1a7d068d1b6fc410ddeb
laiwufa <80929954@yonghui.cn>
2019-12-27 17:47:09 +0800

    add svnutil

--- java8/src/main/java/com/zilanghuo/java8/LambdaDemo.java
+++ java8/src/main/java/com/zilanghuo/java8/LambdaDemo.java
@@ -1,7 +1,13 @@
 package com.zilanghuo.java8;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+
+import javax.xml.crypto.Data;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 
 /**
  * @author laiwufa
@@ -11,10 +17,22 @@ public class LambdaDemo {
 
     public static void main(String[] args) {
 
-        String[] arr = {"xiao", "nihao", "dfajdf"};
-        // Arrays.sort(arr, (name1, name2) -> name1.length() - name2.length());
-        Arrays.sort(arr, Comparator.comparingInt(String::length));
-      //  Arrays.sort(arr, StringOrder::byLength);
-        System.out.println(Arrays.toString(arr));
+        int[] array = {23,43,56,97,32};
+        Arrays.stream(array).reduce((x,y) -> x+y).ifPresent(s -> System.out.println(s));
+        Arrays.stream(array).reduce(Integer::sum).ifPresent(s -> System.out.println(s));
+       // Arrays.stream(array).reduce(StatisticsUtility::addIntData).ifPresent(s -> System.out.println(s));
+
+        long start = 1573286400000L;
+
+
+        Calendar instance = Calendar.getInstance();
+        instance.setTimeInMillis(start);
+        Date date = instance.getTime();
+
+        // 1573286400000
+        // 1574233131530
+        String str = "34324,432,4324";
+        System.out.println(str.substring(0,str.indexOf(",")));
+
     }
 }
